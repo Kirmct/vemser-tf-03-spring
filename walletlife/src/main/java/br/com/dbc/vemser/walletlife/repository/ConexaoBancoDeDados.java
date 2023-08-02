675d2a15c5ec3f256f44af5f6a7de46e4e717575
ViniciusP-Santos <viniciuspereira2244@gmail.com>
2023-08-02 20:00:49 -0300

    Conexao com banco de dados

--- walletlife/src/main/java/br/com/dbc/vemser/walletlife/repository/ConexaoBancoDeDados.java
+++ walletlife/src/main/java/br/com/dbc/vemser/walletlife/repository/ConexaoBancoDeDados.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 
 public class ConexaoBancoDeDados {
 
-    private static final String SERVER = "localhost";
+    private static final String SERVER = "vemser-hml.dbccompany.com.br";
     private static final String PORT = "1521"; // Porta TCP padrão do Oracle
     private static final String DATABASE = "xe";
 
     // Configuração dos parâmetros de autenticação
-    private static final String USER = "system";
+    private static final String USER = "EQUIPE_5";
     private static final String PASS = "oracle";
 
-    private static final String SCHEMA = "VEM_SER";
+    private static final String SCHEMA = "EQUIPE_5";
 
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:oracle:thin:@" + SERVER + ":" + PORT + ":" + DATABASE;
